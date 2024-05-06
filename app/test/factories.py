4328ab01c29e7ab29ef1a48870e496e0dfb4f123
seigo <utti.nbst.s@gmail.com>
2024-05-06 23:26:57 +0900

    公演を一括追加する機能を追加

--- app/test/factories.py
+++ app/test/factories.py
@@ -158,6 +158,26 @@ group2 = schemas.GroupCreate(
     type=schemas.GroupType.play
 )
 
+# 変数
+group3 = schemas.GroupCreate(
+    id="test_1",
+    groupname="テストグループ",
+    title="TEST_TEST",
+    description="TESTです",
+    enable_vote=True,
+    twitter_url=None,
+    instagram_url=None,
+    stream_url=None,
+    public_thumbnail_image_url=None,
+    public_page_content_url="<html><h1>宣伝ページ</h1></html>",
+    private_page_content_url="<html><h1>プライベート</h1></html>",
+    floor=4,
+    place="LL教室",
+    type=schemas.GroupType.test
+)
+
+
+
 group1_update = schemas.GroupUpdate(
     id="28r",
     groupname="2年8組",
