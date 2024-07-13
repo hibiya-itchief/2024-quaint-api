d2884eacddd95f24fc9cb6f512c63a432d3f9031
ekkekuru2 <ekke@ekke.jp>
2024-07-14 02:35:11 +0900

    chore: 以前からあったデータベースのoverrideおよびfixtureにコメントを追加

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -9,12 +9,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
-DATABASE_URI = "mysql://"+ settings.mysql_user +":"+ settings.mysql_password +"@"+ settings.db_host +"/quaint-app"
+TEST_DATABASE_URI = "mysql://"+ settings.mysql_user +":"+ settings.mysql_password +"@"+ settings.db_host +"/quaint-app-test"
 
-@pytest.fixture(scope="function")
+### データベースのfixture
+# 各テストケースが実行されるごとに、データベースを他のテストの影響を受けない(=全て削除された)クリーンな状態にしたうえで、DBへの接続を提供する
+
+### ./utils/overrides.pyの app.dependency_overrides[get_db] = override_get_db と混同しそうだが
+## test/conftest.pyでは
+# テスト用テーブルが無ければ作成、各テストケース終了後のクリーンアップといった、「前処理・後処理」を担当している
+# (必要であれば)加えて、テスト「する」側である、app/test/以下で定義されているテスト関数たちにテスト用DBへの接続を提供している
+# (テストの前処理として、APIを経由せずデータベースに直接レコードを追加したかったりするので)
+# Pytestモジュールのfixture関数をデコレートすることで実現している
+## 対してtest/utils/overrides.pyでは
+# テスト「される」側である、app/main.pyで定義されている各エンドポイントなどが利用するDB接続をオーバーライドしている
+# app/main.pyで定義されている各エンドポイントはFastAPIライブラリのFastAPIクラスのメンバ関数であるFastAPI.get()などをデコレートしているので
+# FastAPIクラスのインスタンスであるapp.main.appに対して、app.dependency_overrides[get_db] = override_get_db などとすることで、FastAPIライブラリが良しなにやってくれている
+
+@pytest.fixture(scope="function",autouse=True)
 def db() -> Generator[Session, None, None]:
-    if not database_exists(DATABASE_URI):
-        create_database(DATABASE_URI)
+    if not database_exists(TEST_DATABASE_URI):
+        create_database(TEST_DATABASE_URI)
 
     Base.metadata.create_all(bind=engine)
     session = TestingSessionLocal()
--- app/test/utils/overrides.py
+++ app/test/utils/overrides.py
@@ -6,6 +6,20 @@ from app.main import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+### DBのオーバーライド
+# テストでは一旦データベースをまっさらな状態にしテスト用のデータを投入する、ということを繰り返す
+# そのため開発者のPCで動作している開発用のデータベースなどがぐちゃぐちゃになることを防ぐためテスト用の別のテーブルに接続させる
+
+### ../conftest.py の @pytest.fixtureと混同しそうだが
+## test/utils/overrides.pyでは
+# テスト「される」側である、app/main.pyで定義されている各エンドポイントなどが利用するDB接続をオーバーライドしている
+# app/main.pyで定義されている各エンドポイントはFastAPIライブラリのFastAPIクラスのメンバ関数であるFastAPI.get()などをデコレートしているので
+# FastAPIクラスのインスタンスであるapp.main.appに対して、app.dependency_overrides[get_db] = override_get_db などとすることで、FastAPIライブラリが良しなにやってくれている
+## 対して、test/conftest.pyでは
+# テスト用テーブルが無ければ作成、各テストケース終了後のクリーンアップといった、「前処理・後処理」を担当している
+# (必要であれば)加えて、テスト「する」側である、app/test/以下で定義されているテスト関数たちにテスト用DBへの接続を提供している
+# (テストの前処理として、APIを経由せずデータベースに直接レコードを追加したかったりするので)
+# これまたPytestモジュールのfixture関数をデコレートすることで実現している
 TEST_DATABASE_URI = "mysql://"+ settings.mysql_user +":"+ settings.mysql_password +"@"+ settings.db_host +"/quaint-app-test"
 
 engine = create_engine(TEST_DATABASE_URI)
