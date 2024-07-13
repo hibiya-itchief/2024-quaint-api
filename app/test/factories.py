0369f7e52c02011b7fb59349074c059115ddc631
ekkekuru2 <ekke@ekke.jp>
2024-07-14 03:14:32 +0900

    chore: 明らかに現行の仕様ではないログインに関するfactoryを削除

--- app/test/factories.py
+++ app/test/factories.py
@@ -6,49 +6,6 @@ from app import models
 from app import schemas
 from app.test.utils.overrides import TestingSessionLocal
 
-class Admin_UserCreateByAdmin():
-    username = "admin"
-    password = "password"
-    is_student=True
-    is_family=False
-    is_active=False
-    password_expired=False
-
-class hogehoge_UserCreateByAdmin():
-    username = "hoge_hoge"
-    password = "password"
-    is_student=False
-    is_family=False
-    is_active=False
-    password_expired=False
-class passwordexpired_UserCreateByAdmin():
-    username = "hoge_hoge"
-    password = "password"
-    is_student=False
-    is_family=False
-    is_active=False
-    password_expired=True
-class active_UserCreateByAdmin():
-    username = "active_hoge_hoge"
-    password = "password"
-    is_student=False
-    is_family=False
-    is_active=True
-    password_expired=False
-class inactive_UserCreateByAdmin():
-    username = "active_hoge_hoge"
-    password = "password"
-    is_student=False
-    is_family=False
-    is_active=False
-    password_expired=False
-class active_student_UserCreateByAdmin():
-    username = "active_hoge_hoge"
-    password = "password"
-    is_student=True
-    is_family=False
-    is_active=True
-    password_expired=False
 class tag1_TagCreateByAdmin():
     tagname="タグ1"
 class tag2_TagCreateByAdmin():
