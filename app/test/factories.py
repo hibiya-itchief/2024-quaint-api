3f37fbbce65465dfaa9e24e977822f1e996e171d
seigo <utti.nbst.s@gmail.com>
2024-07-25 12:09:51 +0900

    テスト追加

--- app/test/factories.py
+++ app/test/factories.py
@@ -12,6 +12,36 @@ from app.test.utils.overrides import TestingSessionLocal
 def authheader(user:Dict):
     return {'Authorization': json.dumps(user)}
 
+valid_student_user={
+    "iss": "https://login.microsoftonline.com/158e6d17-f3d5-4365-8428-26dfc74a9d27/v2.0",
+    "groups": [
+        "865bb05d-cb7d-4919-b18d-8b977ec0499b", # quaint-students
+    ],
+    "name": "studentfortest",
+    "oid": "25e3cf28-e627-4dfe-b5dd-bdcbe73117e1", # random
+    "sub": "BEGuhvmm8LkWHLxEK9TxDkVaMvK3nDGq6ak79HPGLsd", # random
+}
+
+valid_parent_user={
+    "iss": "https://login.microsoftonline.com/158e6d17-f3d5-4365-8428-26dfc74a9d27/v2.0",
+    "groups": [
+        "ecd46dae-d84b-42d8-9357-ac24d480a168", # quaint-parents
+    ],
+    "name": "adminfortest",
+    "oid": "25e3cf28-e627-4dfe-b5dd-bdcbe73117e1", # random
+    "sub": "BEGuhvmm8LkWHLxEK9TxDkVaMvK3nDGq6ak79HPGLsd", # random
+}
+
+valid_guest_user={
+    "iss": "https://login.microsoftonline.com/158e6d17-f3d5-4365-8428-26dfc74a9d27/v2.0",
+    "groups": [
+        "94c45b57-680c-4b5b-a98b-d78f1fd90d71", # quaint-guest
+    ],
+    "name": "adminfortest",
+    "oid": "25e3cf28-e627-4dfe-b5dd-bdcbe73117e1", # random
+    "sub": "BEGuhvmm8LkWHLxEK9TxDkVaMvK3nDGq6ak79HPGLsd", # random
+}
+
 valid_admin_user={
     "iss": "https://login.microsoftonline.com/158e6d17-f3d5-4365-8428-26dfc74a9d27/v2.0",
     "groups": [
@@ -216,7 +246,22 @@ group3 = schemas.GroupCreate(
     type=schemas.GroupType.test
 )
 
-
+club_1 = schemas.GroupCreate(
+    id="soccer_club",
+    groupname="テストグループ",
+    title="TEST_TEST",
+    description="TESTです",
+    enable_vote=False,
+    twitter_url=None,
+    instagram_url=None,
+    stream_url=None,
+    public_thumbnail_image_url=None,
+    public_page_content_url="<html><h1>宣伝ページ</h1></html>",
+    private_page_content_url="<html><h1>プライベート</h1></html>",
+    floor=4,
+    place="LL教室",
+    type=schemas.GroupType.club
+)
 
 group1_update = schemas.GroupUpdate(
     id="28r",
